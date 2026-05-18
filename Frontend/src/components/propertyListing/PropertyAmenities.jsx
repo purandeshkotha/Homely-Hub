9aaf8dbf0dd52f0c159c51d90127c5a6029edcae
purandeshkotha <purandesh268@gmail.com>
2026-05-18 19:28:18 +0530

    Fix amenities UI - clean 2-column grid layout with card styling

--- Frontend/src/components/propertyListing/PropertyAmenities.jsx
+++ Frontend/src/components/propertyListing/PropertyAmenities.jsx
@@ -3,20 +3,18 @@ import React from 'react'
 const PropertyAmenities = ({amenities}) => {
   return (
     <>
-      <h2 className='property-amenities'>what this place offers</h2>
+      <h2 className='property-amenities'>What this place offers</h2>
       <div className='amenities'>
-        {amenities.map((amenity,index) =>(
-            <p key={index}>
-                <span className='material-symbols-outlined'>
-                    {amenity.icon}
-                </span>
-                <span>{amenity.name}</span>
-            </p>
+        {amenities.map((amenity, index) => (
+          <div key={index} className='amenity-item'>
+            <span className='material-symbols-outlined amenity-icon'>
+              {amenity.icon}
+            </span>
+            <span className='amenity-name'>{amenity.name}</span>
+          </div>
         ))}
       </div>
-    
     </>
-    
   )
 }
 
